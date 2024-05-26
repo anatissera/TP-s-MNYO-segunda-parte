42bcaa80956c14757dd3e2fb539bead5a3e5fec2
Matteo Musacchio <mattemusaa@gmail.com>
2024-05-26 03:44:34 -0300

    todos los graficos del 2

--- TP3/TP3_P2.py
+++ TP3/TP3_P2.py
@@ -27,7 +27,7 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-
+plt.rcParams["font.family"] = "serif"
 # Cada imagen es una matriz de p × p que puede representarse como un vector x ∈ Rp∗p. A su vez, es posible armar un matriz de datos apilando los vectores de cada imagen generando una matriz de n × (p ∗ p).
 # tengo una carpeta con varias imagenes .jpeg
 # img00.jpeg, img01.jpeg, img02.jpeg, ..., img0n.jpeg
@@ -38,9 +38,8 @@ from PIL import Image
 
 # cargar las imagenes
 images = []
-for i in range(19):
+for i in range(0,19):
     img = Image.open(f"TP3\datasets_imgs/img{i}.jpeg")
-    img = img.resize((100, 100))
     img = np.array(img)
     images.append(img)
 
@@ -62,55 +61,152 @@ X = images.reshape(n, p*p)
 # Descomposición en valores singulares
 U, S, Vt = np.linalg.svd(X)
 
-# mostrar los valores singulares
-fig, ax = plt.subplots()
-ax.plot(S)
-ax.set_title("Valores singulares")
-plt.show()
+def plot_singular_values(S):
+    # Valores singulares {𝜎𝑖}19
+    # 𝑖=1 de 𝐴 en escala semilogarítmica
+    # respecto al eje de ordenadas. Se observa que se hallan ordenados
+    # de forma decreciente y que el primer valor singular 𝜎1 duplica
+    # al siguiente valor singular 𝜎2, frente a un menor decrecimiento
+    # valor a valor para 𝑖 subsiguientes.
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    # cambiar la fuente a serif
+    plt.plot(S, marker="o")
+    plt.yscale("log")
+    plt.ylabel("Valor singular 𝜎i")
+    plt.xlabel("i")
+
+    #Proporción de la suma acumulada de {𝜎𝑖}19
+    # 𝑖=1 de 𝐴
+    # respecto de la desviación 𝜎 total (ver 12). Se representa el
+    # aporte de información de las primeras 𝑟 componentes de la
+    # descomposición SVD de 𝑋. En particular, se expone que,
+    # tomando las primeras 5 componentes 𝐮𝑖𝜎𝑖𝐯⊤, se concentra el
+    # 50% de la información no redundante de 𝑋.
+
+    # Calcular la proporción de la suma acumulada de los valores singulares
+    cumsum = np.cumsum(S)
+    cumsum /= cumsum[-1]
+
+
+    plt.subplot(1, 2, 2)
+    plt.plot(cumsum, marker="o", color="slateblue")
+    plt.title("Proporción de la suma acumulada")
+
+    plt.show()
+
+    # mostrar los valores singulares
+# def plot_singular_values(S):
+#     # Valores singulares {𝜎𝑖}19
+#     # 𝑖=1 de 𝐴 en escala semilogarítmica
+#     # respecto al eje de ordenadas. Se observa que se hallan ordenados
+#     # de forma decreciente y que el primer valor singular 𝜎1 duplica
+#     # al siguiente valor singular 𝜎2, frente a un menor decrecimiento
+#     # valor a valor para 𝑖 subsiguientes.
+#     fig, ax = plt.subplots()
+#     ax.plot(S)
+#     ax.set_title("Valores singulares")
+#     plt.show()
+
+plot_singular_values(S)
 
 # medir la similaridad entre un par de muestras xi, xj
 def similarity(xi, xj, sigma=1):
     return np.exp(-np.linalg.norm(xi - xj)**2 / (2 * sigma**2))
 
 def error_approximation(X, X_reconstructed):
+#    error respecto a las imágenes originales que induce su reconstrucción a partir de los primeros 8 autovectores {𝐮𝑖}8 𝑖=1 (primera mitad de la base de autovectores 𝑈𝐴) en contraste con el mismo experimento llevado a cabo con las últimas 8 componentes de 𝑈𝐴 {𝐮𝑖}16 𝑖=9 (segunda mitad de la base de autovectores)
     # Error =
 # ‖𝑋𝑖 − ̃ 𝑋𝑖‖𝐹 /
 # ‖𝑋𝑖‖𝐹
-   return (np.linalg.norm(X - X_reconstructed, ord=2) / np.linalg.norm(X, ord=2))*100
+    return np.linalg.norm(X - X_reconstructed) / np.linalg.norm(X) * 100
 
 # Visualizar en forma matricial p × p las imágenes reconstruidas luego de compresión con distintos valores de d dimensiones ¿Qué conclusiones pueden sacar?
-d_values = [2, 6, 10,14, 16, 50]
-fig, axs = plt.subplots(2, 3)
-for d, ax in zip(d_values, axs.flatten()):
-    Z = np.dot(X, Vt[:d].T)
-    X_reconstructed = np.dot(Z, Vt[:d])
-    images_reconstructed = X_reconstructed.reshape(n, p, p)
-    ax.imshow(images_reconstructed[0], cmap="gray")
-    # mostrar el porcentaje de error en cada imagen
-    error = error_approximation(X[0], X_reconstructed[0])
-    ax.set_title(f"d={d} error={error:.2f}%")
-plt.show()
+
+def plot_images_reconstructed(X, Vt, d_values):
+    fig, axs = plt.subplots(2, 3)
+    for d, ax in zip(d_values, axs.flatten()):
+        Z = np.dot(X, Vt[:d].T)
+        X_reconstructed = np.dot(Z, Vt[:d])
+        images_reconstructed = X_reconstructed.reshape(n, p, p)
+        ax.imshow(images_reconstructed[0], cmap="gray")
+        # mostrar el porcentaje de error en cada imagen
+        error = error_approximation(X[0], X_reconstructed[0])
+        ax.set_title(f"d={d} error={error:.2f}%")
+    plt.show()
+
+d_values = [2, 6, 10, 14, 16, 50]
+plot_images_reconstructed(X, Vt, d_values)
 
 # Graficar porcentaje de error de aproximacion para cada una de las imagenes a las que se les aplico SVD truncado con d = 6
-d = 6
-Z = np.dot(X, Vt[:d].T)
-X_reconstructed = np.dot(Z, Vt[:d])
-images_reconstructed = X_reconstructed.reshape(n, p, p)
+def plot_error_approximation(X, Vt, d1, d2):
+    fig, ax = plt.subplots()
+    error1 = []
+    error2 = []
+    for i in range(n):
+        Z = np.dot(X[i], Vt[:d1].T)
+        X_reconstructed = np.dot(Z, Vt[:d1])
+        error1.append(error_approximation(X[i], X_reconstructed))
 
-# Calcular el porcentaje de error de aproximacion
-error = []
-for i in range(n):
-    error.append(error_approximation(X[i], X_reconstructed[i]))
+        Z = np.dot(X[i], Vt[:d2].T)
+        X_reconstructed = np.dot(Z, Vt[:d2])
+        error2.append(error_approximation(X[i], X_reconstructed))
+
+    ax.bar(np.arange(n), error1, label=f"d={d1}", color="salmon")
+    ax.bar(np.arange(n), error2, label=f"d={d2}", color="slateblue")
+    ax.set_title("Error de aproximación")
+    ax.legend()
+    plt.show()
+
+plot_error_approximation(X, Vt, 4,8) 
+# intente copiar el de segundo pero me parece que no es el mismo
 
 
-# Graficar en barras el error de aproximacion
-fig, ax = plt.subplots()
-ax.bar(range(n), error)
-ax.set_title("Error de aproximación")
-plt.show()
+# usar el dataset 2
+# cargar las imagenes
+images = []
+for i in range(8):
+    img = Image.open(f"TP3\datasets_imgs_02/img0{i}.jpeg")
+    img = np.array(img)
+    images.append(img)
+
+# convertir las imagenes a un vector
+images = np.array(images)
+n, p, _ = images.shape
+X = images.reshape(n, p*p)
+
+# Descomposición en valores singulares
+U, S, Vt = np.linalg.svd(X)
+
+def plot_minimum_dimension(X, error_threshold):
+    # Dado el dataset dataset_imagenes2.zip encontrar d, el número mínimo de dimensiones a las que se
+    # puede reducir la dimensionalidad de su representación mediante valores singulares tal que el error de
+    # cada imagen comprimida y su original no exceda el 10% bajo la norma de Frobenius. Utilizando esta
+    # ultima representación aprendida con el dataset 2 ¿Qué error de reconstrucción obtienen si utilizan la
+    # misma compresión (con la misma base de d dimensiones obtenida del dataset 2) para las imagenes
+    # dataset_imagenes1.zip?
+    
+    # hacer un grafico de barras, en el eje Y que muestre dimensiones, y en el eje X el numero de imagen. Con esto vamos a ver el numero minimo de dimensiones para que cada imagen tenga un error menor al 10%
+    fig, ax = plt.subplots()
+    d_values = np.arange(1, X.shape[1])
+    for i in range(8):
+        error = []
+        for d in d_values:
+            Z = np.dot(X[i], Vt[:d].T)
+            X_reconstructed = np.dot(Z, Vt[:d])
+            error.append(error_approximation(X[i], X_reconstructed))
+            if(error[-1] < error_threshold):
+                break
+        ax.bar(i, d, color="slateblue")
+    ax.set_title("Número mínimo de dimensiones")
+    plt.show()
+
+plot_minimum_dimension(X, 10)
 
 
-#analizar similaridad entre los distintos valores de d
+
+
+# # analizar similaridad entre los distintos valores de d
 # for d in d_values:
 #     Z = np.dot(X, Vt[:d].T)
 #     similarity_X = np.exp(-np.linalg.norm(X - X[:, None], axis=2)**2 / (2 * 1))
@@ -123,7 +219,7 @@ plt.show()
 #     ax[1].set_title(f"Similarity Z d={d}")
 #     plt.show()
 
-# Utilizando compresión con distintos valores de d medir la similaridad entre pares de imágenes (con alguna métrica de similaridad que decida el autor) en un espacio de baja dimensión d. Analizar cómo la similaridad entre pares de imágenes cambia a medida que se utilizan distintos valores de d. Cuales imágenes se encuentran cerca entre si? Alguna interpretación al respecto? Ayuda: ver de utilizar una matriz de similaridad para visualizar todas las similaridades par-a-par juntas.
+# # Utilizando compresión con distintos valores de d medir la similaridad entre pares de imágenes (con alguna métrica de similaridad que decida el autor) en un espacio de baja dimensión d. Analizar cómo la similaridad entre pares de imágenes cambia a medida que se utilizan distintos valores de d. Cuales imágenes se encuentran cerca entre si? Alguna interpretación al respecto? Ayuda: ver de utilizar una matriz de similaridad para visualizar todas las similaridades par-a-par juntas.
 
 # # medir la similaridad entre un par de muestras xi, xj
 # def similarity(xi, xj, sigma=1):
