6ab8031582b78a3b956713dce7b49b56aaee9987
anatissera <atissera@udesa.edu.ar>
2024-05-31 12:36:20 -0300

    sigo probando el 1.3

--- TP3/P1_3.py
+++ TP3/P1_3.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 
 from P1_1 import load_data, normalize_dataset, pca_with_svd
+from sklearn.model_selection import train_test_split
 
 def pseudo_inverse(S_d):
     S_d_inv = np.copy(S_d)
@@ -54,27 +55,6 @@ def svd_least_squares_PCA(X, y, d):
     return beta, error
 
 
-# def plot_prediction_errors(X, y):
-#     errors = []
-#     dims = range(1, X.shape[1] + 1)
-
-#     for d in dims:
-#         beta, error = svd_least_squares(X, y, d)
-#         errors.append(error)
-       
-#     best_dimension = dims[np.argmin(errors)]   
-    
-#     plt.figure(figsize=(12, 6))
-#     plt.plot(dims, errors, 'o-', markersize=2.5, color="darkcyan", linewidth=y)
-#     plt.xlabel('Dimensiones')
-#     plt.ylabel('Error de predicción (norma 2)')
-#     plt.title('Error de predicción para diferentes dimensiones')
-#     plt.grid(True)
-#     plt.show()
-    
-#     print(f"La mejor dimensión es {best_dimension} con un error de {errors[best_dimension-1]}")
-
-
 def svd_least_squares(X, y, d):
     U, S, Vt = np.linalg.svd(X, full_matrices=False)
     U_d = U[:, :d]
@@ -87,6 +67,7 @@ def svd_least_squares(X, y, d):
     error = np.linalg.norm(X @ beta - y) / np.linalg.norm(y)
     
     return beta, error
+
 
 def plot_prediction_errors(X, y, bool=False):
     errors = []
@@ -113,39 +94,72 @@ def plot_prediction_errors(X, y, bool=False):
     
     return best_dimension
 
-def plot_beta_weights(beta):
-    plt.figure(figsize=(12, 6))
-    plt.bar(range(1, len(beta) + 1), beta)
-    plt.title('Pesos del Vector β en el Espacio Original')
-    plt.xlabel('Dimensiones Originales')
-    plt.ylabel('Pesos de β')
+# def plot_singular_values(X, dims):
+    
+def plot_singular_values(X):
+    _, S, _ = np.linalg.svd(X, full_matrices=False)
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, len(S) + 1), S, 'o-', markersize=4, color="blue", linewidth=2)
+    plt.xlabel('Componentes')
+    plt.ylabel('Valor Singular')
+    plt.title('Valores Singulares')
     plt.grid(True)
     plt.show()
 
-def plot_predictions_vs_observations(y, y_pred):
-    plt.figure(figsize=(12, 6))
-    plt.scatter(y, y_pred, c= y, cmap='viridis', label='Predicciones')
-    plt.plot([min(y), max(y)], [min(y), max(y)], color='red', linestyle='--', label='Observaciones')
-    plt.title('Predicciones vs Observaciones Reales')
-    plt.xlabel('Observaciones Reales')
-    plt.ylabel('Predicciones')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+# def plot_beta_weights(beta):
+#     plt.figure(figsize=(12, 6))
+#     plt.bar(range(1, len(beta) + 1), beta)
+#     plt.title('Pesos del Vector β en el Espacio Original')
+#     plt.xlabel('Dimensiones Originales')
+#     plt.ylabel('Pesos de β')
+#     plt.grid(True)
+#     plt.show()
+
+# def plot_predictions_vs_observations(y, y_pred):
+#     plt.figure(figsize=(12, 6))
+#     plt.scatter(y, y_pred, c= y, cmap='viridis', label='Predicciones')
+#     plt.plot([min(y), max(y)], [min(y), max(y)], color='red', linestyle='--', label='Observaciones')
+#     plt.title('Predicciones vs Observaciones Reales')
+#     plt.xlabel('Observaciones Reales')
+#     plt.ylabel('Predicciones')
+#     plt.legend()
+#     plt.grid(True)
+#     plt.show()
     
     
 def main():
     X, y = load_data()
     X = normalize_dataset(X)
-    plot_prediction_errors(X, y)
     
-    best_dimension = plot_prediction_errors(X, y, True)
+    # # plot_prediction_errors(X, y, True)  # este es el que no es con PCA
     
-    beta, _ = svd_least_squares(X, y, best_dimension)
-    y_pred = X @ beta
+    # best_dimension = plot_prediction_errors(X, y) # con PCA
     
-    plot_beta_weights(beta)
-    plot_predictions_vs_observations(y, y_pred)
+    # beta, _ = svd_least_squares(X, y, best_dimension) # uso el que no es con PCA para obtener beta
+    # # beta = X_pseudo_inv @ y
+    
+    # y_pred = X @ beta # ??
+    
+    # plot_beta_weights(beta)
+    # plot_predictions_vs_observations(y, y_pred)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Analizar valores singulares
+    plot_singular_values(X_train)
+
+    # Encontrar la mejor dimensión y calcular el error
+    best_dimension = plot_prediction_errors(X_train, y_train)
+
+    # Obtener el vector beta usando la mejor dimensión
+    beta, _ = svd_least_squares_PCA(X_train, y_train, best_dimension)
+    
+    # Predecir en el conjunto de prueba
+    y_pred = X_test @ beta
+    
+    # Evaluación del error en el conjunto de prueba
+    test_error = np.linalg.norm(y_test - y_pred) / np.linalg.norm(y_test)
+    print(f"Error de predicción en el conjunto de prueba: {test_error}")
 
 
 if __name__ == '__main__':
--- TP3/P1_3borrador.py
+++ TP3/P1_3borrador.py
@@ -16,208 +16,98 @@
 
 # La solución se obtiene entonces como β = X^+ y = V S^+ U^T y. \\
 
-
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-
 from P1_1 import load_data, normalize_dataset, pca_with_svd
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
-# def perform_pca(X_dataset, Y, dims):
-#     pca = PCA(n_components=X_dataset.shape[1])
-#     pca.fit(X_dataset)
-#     V = pca.components_.T
-    
-#     for d in dims:
-#         Vd = V[:, :d]
-#         Z = np.dot(X_dataset, Vd)
-#         model = LinearRegression().fit(Z, Y)
-#         y_pred = model.predict(Z)
+def pseudo_inverse(S_d):
+    S_d_inv = np.copy(S_d)
+    for i in range(len(S_d)):
+        if S_d[i, i] != 0:
+            S_d_inv[i, i] = 1 / S_d[i, i]
+        else:
+            S_d_inv[i, i] = 0
+    return S_d_inv
 
-#     return model, y_pred
-
-def svd_least_squares(X, y, d):
-    # Descomposición en valores singulares (SVD)
+def svd_least_squares_PCA(X, y, d):
     U, S, Vt = np.linalg.svd(X, full_matrices=False)
     V = Vt.T
-        
     U_d = U[:, :d]
     S_d = np.diag(S[:d])
-    VT_d = Vt[:d, :]
+    Vt_d = Vt[:d, :]
     V_d = V[:, :d]
-    
-    # Calcular la pseudo-inversa de S
-    S_inv = np.diag(1 / S_d)
-
-    # Calcular la pseudo-inversa de X
-    X_pseudo_inv = V_d @ S_inv @ U_d.T
-    
-    # PCA
-    X_pca = S_inv @ U_d.T
-
-    # Calcular el vector de parámetros beta
-    # beta = X_pseudo_inv @ y
-    
+    X_pca = pseudo_inverse(S_d) @ U_d.T
     beta = X_pca @ y
-    
-    error = np.linalg.norm(X @ VT_d.T @ beta - y) / np.linalg.norm(y)
-
+    error = np.linalg.norm(X @ Vt_d.T @ beta - y) / np.linalg.norm(y)
     return beta, error
 
+def svd_least_squares(X, y, d):
+    U, S, Vt = np.linalg.svd(X, full_matrices=False)
+    U_d = U[:, :d]
+    S_d = np.diag(S[:d])
+    Vt_d = Vt[:d, :]
+    X_pca_pseudo_inv = Vt_d.T @ pseudo_inverse(S_d) @ U_d.T
+    beta = X_pca_pseudo_inv @ y
+    error = np.linalg.norm(X @ beta - y) / np.linalg.norm(y)
+    return beta, error
 
-
-def pca_analysis(X, y, d_values):
-    results = {}
-    for d in d_values:
-        # Aplicar PCA para reducir la dimensionalidad a d componentes
-        pca = PCA(n_components=d)
-        X_reduced = pca.fit_transform(X)
-
-        # Calcular los parámetros beta usando SVD en el espacio reducido
-        beta = svd_least_squares(X_reduced, y)
-
-        # Calcular las predicciones
-        y_pred = X_reduced @ beta
-
-        # Calcular el error de predicción
-        error = np.linalg.norm(y - y_pred)
-
-        results[d] = {
-            "beta": beta,
-            "error": error,
-            "y_pred": y_pred,
-            "explained_variance_ratio": pca.explained_variance_ratio_
-        }
-
-    return results
-
-
-def plot_prediction_errors(X, y):
+def plot_prediction_errors(X, y, use_PCA=True):
     errors = []
-    dims = range(1, X.shape[1]+1)
-
+    dims = range(1, X.shape[1] + 1)
     for d in dims:
-        beta, error = svd_least_squares(X, y, d)
+        if use_PCA:
+            _, error = svd_least_squares_PCA(X, y, d)
+        else:
+            _, error = svd_least_squares(X, y, d)
         errors.append(error)
-        print(f"Dimensión: {d}, Error de predicción: {error}")
-
+    best_dimension = dims[np.argmin(errors)]
     plt.figure(figsize=(12, 6))
-    plt.plot(dims, errors, marker='o')
+    plt.plot(dims, errors, 'o-', markersize=2.5, color="darkcyan", linewidth=2)
     plt.xlabel('Dimensiones')
     plt.ylabel('Error de predicción (norma 2)')
     plt.title('Error de predicción para diferentes dimensiones')
     plt.grid(True)
     plt.show()
-    
-def leastSquares(matrix, y):
-    # Realizamos la descomposición SVD
-    u, s, v = np.linalg.svd(matrix, full_matrices=False)
-    errors = []
-    dimensions = []
-
-    for d in range(1, matrix.shape[1] + 1):
-        # Realizamos la reducción de dimensiones
-        U_reduced = u[:, :d]
-        S_reduced = np.diag(s[:d])
-        V_reduced = v[:d, :]
-        matrix_svd = U_reduced @ S_reduced @ V_reduced
-        
-        # Calculamos la matriz de coeficientes
-        A = np.linalg.inv(S_reduced) @ U_reduced.T
-        # A = np.linalg.pinv(U_reduced @ S_reduced)
-        
-        # Calculamos los coeficientes
-        beta = A @ y
-        
-        # Calculamos el error
-        error = np.linalg.norm(matrix @ V_reduced.T @ beta - y) / np.linalg.norm(y)
-
-        errors.append(error)
-        dimensions.append(d)
-    
-    best_dimension = dimensions[np.argmin(errors)]    
-            
-    plt.figure()
-    plt.plot(dimensions, errors)
-    plt.title("Error relativo entre normas en función de la dimensión")
-    plt.xlabel("Dimensión")
-    plt.ylabel("% de Error")
-    # plt.ylim(0.75, 0.80)
-    plt.show()
-    
     print(f"La mejor dimensión es {best_dimension} con un error de {errors[best_dimension-1]}")
-    
+    return best_dimension
+
+def plot_singular_values(X):
+    _, S, _ = np.linalg.svd(X, full_matrices=False)
+    plt.figure(figsize=(12, 6))
+    plt.plot(range(1, len(S) + 1), S, 'o-', markersize=4, color="purple", linewidth=2)
+    plt.xlabel('Dimensiones')
+    plt.ylabel('Valor singular')
+    plt.title('Valores singulares de las dimensiones')
+    plt.grid(True)
+    plt.show()
 
 def main():
-    # X, y = load_data()
-    # X_normalized = normalize_dataset(X)
-
-    # # Definir los valores de d a considerar
-    # dims = [2, 6, 10, X.shape[1]]
-    
     X, y = load_data()
     X = normalize_dataset(X)
-    leastSquares(X, y)
-    plot_prediction_errors(X, y)
+    
+    # Separar los datos en conjunto de entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # # Realizar el análisis de PCA y calcular los parámetros beta
-    # results = pca_analysis(X_normalized, y, dims)
+    best_dimension = plot_prediction_errors(X_train, y_train, use_PCA=True)
 
-    # # Determinar la mejor dimensión d que minimiza el error de predicción
-    # best_d = min(results, key=lambda d: results[d]['error'])
-    # best_model = results[best_d]
+    beta, _ = svd_least_squares(X_train, y_train, best_dimension)
+    
+    test_error = np.linalg.norm(X_test @ beta - y_test) / np.linalg.norm(y_test)
+    print(f"Error en el conjunto de prueba: {test_error}")
+    
+    # plot_singular_values(beta)
+    
+    # pesos asignados a cada dimensión original
+    plt.figure(figsize=(12, 6))
+    plt.bar(range(1, len(beta) + 1), beta, color='lightblue')
+    plt.xlabel('Dimensiones originales')
+    plt.ylabel('Pesos del vector beta')
+    plt.title('Pesos asignados a cada dimensión original en el vector beta')
+    plt.grid(True)
+    plt.show()
 
-    # print(f"Mejor dimensión d: {best_d}")
-    # print(f"Error de predicción: {best_model['error']}")
-    # print(f"Vector beta: {best_model['beta']}")
-    # print(f"Varianza explicada por componente: {best_model['explained_variance_ratio']}")
-
-    # # Resolver el problema de cuadrados mínimos en el espacio original X
-    # beta_original = svd_least_squares(X_normalized, y)
-    # print(f"Vector beta en el espacio original: {beta_original}")
-
-    # # Graficar los errores de predicción para cada valor de d
-    # errors = [results[d]['error'] for d in dims]
-    # plt.figure(figsize=(10, 6))
-    # plt.plot(dims, errors, marker='o')
-    # plt.title('Error de Predicción vs Dimensión d')
-    # plt.xlabel('Dimensión d')
-    # plt.ylabel('Error de Predicción')
-    # plt.grid(True)
-    # plt.show()
-
-    # # Graficar la varianza explicada acumulada para el mejor modelo
-    # explained_variance_ratio = np.cumsum(best_model['explained_variance_ratio'])
-    # plt.figure(figsize=(10, 6))
-    # plt.plot(range(1, best_d + 1), explained_variance_ratio, marker='o')
-    # plt.title('Varianza Explicada Acumulada')
-    # plt.xlabel('Número de Componentes Principales')
-    # plt.ylabel('Varianza Explicada Acumulada')
-    # plt.grid(True)
-    # plt.show()
-
-    # # Graficar los pesos del vector beta en el espacio original
-    # plt.figure(figsize=(10, 6))
-    # plt.bar(range(1, len(beta_original) + 1), beta_original)
-    # plt.title('Pesos del Vector β en el Espacio Original')
-    # plt.xlabel('Dimensiones Originales')
-    # plt.ylabel('Pesos de β')
-    # plt.grid(True)
-    # plt.show()
-
-    # # Graficar las predicciones vs las observaciones reales para el mejor modelo
-    # y_pred_best = best_model['y_pred']
-    # plt.figure(figsize=(10, 6))
-    # plt.scatter(y, y_pred_best, c = y, cmap='viridis')
-    # plt.plot([min(y), max(y)], [min(y), max(y)], color='red', linestyle='--')
-    # plt.title('Predicciones vs Observaciones Reales')
-    # plt.xlabel('Observaciones Reales')
-    # plt.ylabel('Predicciones')
-    # plt.grid(True)
-    # plt.show()
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
